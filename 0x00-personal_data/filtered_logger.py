34b19c6d023795d2d1482b11cc1ba6182d2ecc23
123harri <harrietwanjiku90@gmail.com>
2024-08-01 16:43:32 +0000

    a

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+"""
+Filtered Logger Module
+"""
+
+import logging
+import re
+import os
+import mysql.connector
+from typing import List, Tuple
+
+PII_FIELDS: Tuple[str, ...] = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """Returns the log message obfuscated"""
+    pattern = rf'({"|".join(fields)})=.*?{separator}'
+    return re.sub(pattern, lambda m: f'{m.group(1)}={redaction}{separator}',
+                  message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.msg, self.SEPARATOR)
+        return super().format(record)
+
+
+def get_logger() -> logging.Logger:
+    """Creates a logger"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Connects to the database and returns the connection object"""
+    user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    database = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    return mysql.connector.connect(
+        user=user,
+        password=password,
+        host=host,
+        database=database
+    )
+
+
+def main() -> None:
+    """Main function to read and filter data"""
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    fields = [i[0] for i in cursor.description]
+
+    logger = get_logger()
+
+    for row in cursor:
+        message = "; ".join([f"{field}={value}" for field, value in zip(fields, row)])
+        logger.info(message)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
